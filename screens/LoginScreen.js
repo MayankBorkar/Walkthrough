25208212095c0665f2da826ff9280ff3ae8bea56
MayankBorkar <mayankborkar190@gmail.com>
2023-03-20 19:28:05 +0530

    Authentication Error Handling

---
+++ screens/LoginScreen.js
@@ -0,0 +1,33 @@
+import AuthContent from '../components/Auth/AuthContent';
+import {login} from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import {useState} from "react";
+import {Alert} from "react-native";
+
+function LoginScreen() {
+
+    const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+    async function loginHandler({email, password}) {
+        setIsAuthenticating(true);
+
+        try {
+            await login(email, password);
+        } catch (error) {
+            Alert.alert(
+                'Authentication failed!',
+                'Could not log you in. Please check your credentials or try again later!'
+            )
+        }
+        // await login(email, password);
+        setIsAuthenticating(false);
+    }
+
+    if (isAuthenticating) {
+        return <LoadingOverlay message="Logging you in..."/>;
+    }
+
+    return <AuthContent isLogin onAuthenticate={loginHandler}/>;
+}
+
+export default LoginScreen;
---
+++ screens/SignupScreen.js
@@ -0,0 +1,34 @@
+import AuthContent from '../components/Auth/AuthContent';
+import {useState} from "react";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import {createUser} from "../util/auth";
+import {Alert} from "react-native";
+
+function SignupScreen() {
+    const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+    async function signupHandler({email, password}) {
+        setIsAuthenticating(true);
+
+        try {
+            await createUser(email, password);
+        } catch (error) {
+            Alert.alert(
+                'Authentication failed!',
+                'Could not create user, please check your input and try again later.'
+            )
+        }
+
+
+        setIsAuthenticating(false);
+    }
+
+    if (isAuthenticating) {
+        return <LoadingOverlay message="Creating User..."/>;
+    }
+
+
+    return <AuthContent onAuthenticate={signupHandler}/>;
+}
+
+export default SignupScreen;
